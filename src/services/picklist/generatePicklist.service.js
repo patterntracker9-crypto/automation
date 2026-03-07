94b8edf0686cd84e6d277055ec23ae3d1abce92f
patterntracker9-crypto <patterntracker9@gmail.com>
2026-03-07 13:54:34 +0530

    service updated

--- src/services/picklist/generatePicklist.service.js
+++ src/services/picklist/generatePicklist.service.js
@@ -40,7 +40,7 @@ const generatePicklist = async (channel) => {
     throw new ApiError(400, 'channel is required');
   }
 
-  const browser = await chromium.launch({ headless: false });
+  const browser = await chromium.launch({ headless: false,args: ["--no-sandbox", "--disable-setuid-sandbox"] });
   const context = await browser.newContext();
   const page = await context.newPage();
   const email = process.env.EMAIL;
@@ -299,3 +299,4 @@ const generatePicklist = async (channel) => {
 };
 
 export { generatePicklist };
+
