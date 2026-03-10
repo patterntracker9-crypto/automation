36b440784a8ddb33ce1baed1ab3da46235d514c3
patterntracker9-crypto <patterntracker9@gmail.com>
2026-03-10 10:48:12 +0530

    headless flase

--- src/services/picklist/generatePicklist.service.js
+++ src/services/picklist/generatePicklist.service.js
@@ -378,7 +378,7 @@ const generatePicklist = async (channel, socketId = null) => {
   emitToClient('launch', 'Launching browser...');
 
   const browser = await chromium.launch({
-    headless: true, // Production me headless: true rakhein
+    headless: false, // Production me headless: true rakhein
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
   });
 
@@ -659,3 +659,4 @@ const generatePicklist = async (channel, socketId = null) => {
 };
 
 export { generatePicklist };
+
